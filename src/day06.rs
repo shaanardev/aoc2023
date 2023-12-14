615fbcd6d56bcd469d2c02d8f268eafab4f46640
Shaan Arora <shaanar2934@gmail.com>
2023-12-14 20:45:14 +1100

    AOC - 7 - Add Day 6 Part 2

--- src/day06.rs
+++ src/day06.rs
@@ -1,37 +1,15 @@
 use crate::Solution;
 
 #[derive(Debug)]
-pub struct Races {
+pub struct Race {
     times: Vec<i64>,
     distances: Vec<i64>
 }
-
-
-#[derive(Debug)]
-pub struct Day06;
-impl Solution for Day06 {
-    type ParsedInput = Races;
-
-    fn parse_input(input_lines: &str) -> Self::ParsedInput {
-        let lines: Vec<String> = input_lines.lines().map(String::from).collect();
-        
-        let times = lines[0].split_once(":").unwrap();
-        let times_values: Vec<i64> = times.1.split_whitespace().map(|x| x.parse().unwrap()).collect();
-
-        let dists = lines[1].split_once(":").unwrap();
-        let dists_values: Vec<i64> = dists.1.split_whitespace().map(|x| x.parse().unwrap()).collect();
-
-        assert_eq!(times_values.len(), dists_values.len());
-        Races { 
-            times: times_values, 
-            distances: dists_values  
-        }
-    }
-
-    fn part_one(parsed_input: &mut Self::ParsedInput) -> String {
+impl Race {
+    pub fn cal_ways_to_win(&self) -> Vec<i64> {
         let mut counts: Vec<i64> = Vec::new();
-        for (idx, t) in parsed_input.times.iter().enumerate() {
-            let dist = parsed_input.distances[idx];
+        for (idx, t) in self.times.iter().enumerate() {
+            let dist = self.distances[idx];
             let mut count = 0;
             for v in 1..=*t {
                 let curr_dist = v * (t - v);
@@ -42,10 +20,69 @@ impl Solution for Day06 {
             counts.push(count);
         }
 
+        counts
+    }
+}
+
+
+#[derive(Debug)]
+pub struct Day06;
+impl Solution for Day06 {
+    type ParsedInput = Vec<Race>;
+
+    fn parse_input(input_lines: &str) -> Self::ParsedInput {
+        let lines: Vec<String> = input_lines.lines().map(String::from).collect();
+        let race_one = parse_for_p1(&lines);
+        let race_two = parse_for_p2(&lines);
+    
+        vec![race_one, race_two]
+    }
+
+    fn part_one(parsed_input: &mut Self::ParsedInput) -> String {
+        let race = &parsed_input[0];
+
+        let counts = race.cal_ways_to_win();
+
         counts.iter().fold(1, |acc, x| acc * x).to_string()
     }
 
     fn part_two(parsed_input: &mut Self::ParsedInput) -> String {
-        "".to_string()
+        let race = &parsed_input[1];
+        
+        let counts = race.cal_ways_to_win();
+
+        counts.iter().fold(1, |acc, x| acc * x).to_string()
+    }
+}
+
+pub fn parse_for_p1(lines: &Vec<String>) -> Race {
+    let times = lines[0].split_once(":").unwrap();
+    let times_values: Vec<i64> = times.1.split_whitespace().map(|x| x.parse().unwrap()).collect();
+
+    let dists = lines[1].split_once(":").unwrap();
+    let dists_values: Vec<i64> = dists.1.split_whitespace().map(|x| x.parse().unwrap()).collect();
+
+    assert_eq!(times_values.len(), dists_values.len());
+    Race { 
+        times: times_values, 
+        distances: dists_values  
+    }
+}
+
+pub fn parse_for_p2(lines: &Vec<String>) -> Race {
+    let times = lines[0].split_once(":").unwrap();
+    let dists = lines[1].split_once(":").unwrap();
+
+    let mut times_val = String::from(times.1);
+    times_val.retain(|c| !c.is_whitespace());
+    let parsed_time: i64 = times_val.parse().unwrap();
+
+    let mut dist_val = String::from(dists.1);
+    dist_val.retain(|c| !c.is_whitespace());
+    let parsed_dist: i64 = dist_val.parse().unwrap();
+
+    Race { 
+        times: vec![parsed_time], 
+        distances: vec![parsed_dist] 
     }
 }
