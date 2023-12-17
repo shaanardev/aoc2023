c180cee8904659d61bec0e4eac00e0c42324d0e1
Shaan Arora <shaanar2934@gmail.com>
2023-12-17 17:14:15 +1100

    AOC - 11 - Add Day 9 template

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub mod day05;
 pub mod day06;
 pub mod day07;
 pub mod day08;
+pub mod day09;
 
 pub trait Solution {
     type ParsedInput;
@@ -45,6 +46,7 @@ pub fn solve_day(day: &i32) {
         6 => day06::Day06::solve(include_str!("../inputs/6")),
         7 => day07::Day07::solve(include_str!("../inputs/7")),
         8 => day08::Day08::solve(include_str!("../inputs/8")),
+        9 => day09::Day09::solve(include_str!("../inputs/9")),
         _ => panic!("Day not found"),
     };
 }
